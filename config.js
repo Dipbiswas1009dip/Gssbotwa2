df46bee20f7b2352526a3b67ae0d00effab25973
Dipbiswas1009dip <125973899+Dipbiswas1009dip@users.noreply.github.com>
2024-06-27 21:32:04 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaivKaSLI8YZUFyJfW0F'
+global.linkGroup = 'https://chat.whatsapp.com/BUlFsvt3n3iCNMikxmFjWD'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "~~~RTX•DIP~~~";
+global.owner = ['919907298153'];
+global.premium = ['919907298153'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "~~~RTX•DIP~~~";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
-global.autoread = false
+global.autoread = true
 global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
+global.autoTyping = true
+global.autoRecord = true
+global.autoreact = true
 global.onlygroup = false
 global.onlypc = false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
